4d5c4ecc2e8b0d3cd96747f97aabf45176a62a38
ahsanahmedkhan <ahsanahmed.khan@hotmail.com>
2021-09-20 22:50:07 +0500

    Some form component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,54 @@
+import React from 'react';
 import './App.css';
-import { Button } from '@material-ui/core';
+import { Button, ButtonGroup } from '@material-ui/core';
 import SaveIcon from '@material-ui/icons/Save';
+import DeleteIcon from '@material-ui/icons/Delete'
+import Checkbox from '@material-ui/core/Checkbox';
+import FormControlLabel  from '@material-ui/core/FormControlLabel';
+
+function CheckboxExample(){
+  const [checked, setChecked] = React.useState(true)
+  return(
+    
+    <FormControlLabel  
+    control={<Checkbox 
+        checked={checked}
+        icon={<DeleteIcon/>}
+        checkedIcon={<SaveIcon />}
+        onChange={(e) => setChecked(e.target.checked)}
+        inputProps={{
+          'aria-label':'primary checked'  
+        }}
+      />}
+      label="Testing Checkbox"
+    />
+
+      
+    
+  )
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <CheckboxExample />
+      <ButtonGroup variant="contained" color="primary">
+      
       <Button
       startIcon={<SaveIcon />}
       size="small" href="#" variant="contained" color="secondary">
       Save
       </Button>
+
+      <Button
+      startIcon={<DeleteIcon />}
+      size="small" href="#" variant="contained" color="secondary">
+      Discard
+      </Button>
         
-       
+       </ButtonGroup>
+
       </header>
     </div>
   );
