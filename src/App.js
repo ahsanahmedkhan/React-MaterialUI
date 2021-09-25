7ace1ba77c9d9e7d4d0715c26bc61ddd99be56e3
ahsanahmedkhan <ahsanahmed.khan@hotmail.com>
2021-09-25 23:29:01 +0500

    my form

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { Button, ButtonGroup } from '@material-ui/core';
+import { Button, ButtonGroup, Typography } from '@material-ui/core';
 import SaveIcon from '@material-ui/icons/Save';
 import DeleteIcon from '@material-ui/icons/Delete'
 import Checkbox from '@material-ui/core/Checkbox';
@@ -14,15 +14,15 @@ const useStyles = makeStyles({
     background: 'linear-gradient(45deg,#324ea8,#a83250)',
     border: 0,
     marginTop: 15,
-    borderRadius: 15,
+    borderRadius: 5,
     color: 'white',
-    padding: '0, 30px'
+    padding: '10px 68px'
   }
 })
 
 function ButtonStyled(){
   const classes = useStyles();
-  return <Button className={classes.root}>Submit Form</Button>
+  return <Button className={classes.root}>submit</Button>
 }
 
 function CheckboxExample(){
@@ -35,7 +35,8 @@ function CheckboxExample(){
         checkedIcon={<DeleteIcon />}
         onChange={(e) => setChecked(e.target.checked)}
         inputProps={{
-          'aria-label':'primary checked'  
+          'aria-label':'primary checked',
+          color:'primary'  
         }}
       />}
       label="Testing Checkbox"
@@ -49,26 +50,28 @@ function App() {
     <div className="App">
       
     <header className="App-header">
-    
+    <Typography variant="h6" >Education is the basic need of humanity</Typography>
+    <Typography variant="h4">Fill The Form</Typography>
     <TextField variant="outlined" label="First Name" placeholder="Ahsan" color="primary" />
     <br />
-    <TextField variant="outlined" label="Last Name" placeholder="Khan" color="secondary" />
-      <CheckboxExample />
+    <TextField variant="outlined" label="Last Name" placeholder="Khan" color="primary" />
+    <br />
+    <TextField variant="outlined" label="Email" type="email" placeholder="bdoahsan@gmail.com" color="primary" />
+      <CheckboxExample color="primary" />
       
       <ButtonGroup variant="contained" color="primary">
           <Button
-            startIcon={<SaveIcon />} size="small" href="#" variant="contained" color="secondary">
+            startIcon={<SaveIcon />} size="small" href="#" variant="contained" color="primary">
               Save
           </Button>
 
           <Button
-            startIcon={<DeleteIcon />} size="small" href="#" variant="contained" color="secondary">
+            startIcon={<DeleteIcon />} size="small" href="#" variant="contained" color="primary">
               Discard
           </Button>    
       </ButtonGroup>
       <ButtonStyled />
       </header>
-    
       </div>
   );
 }
